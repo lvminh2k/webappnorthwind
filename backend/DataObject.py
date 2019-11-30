d5fa1de0c7a506cef0e7998f990a8a49d55bdfb0
lvminh2k <lvminh2k@gmail.com>
2019-11-30 14:08:26 +0700

    add business object, data object

---
+++ backend/DataObject.py
@@ -0,0 +1,6 @@
+class Customer:
+    def __init__(self, ConnectionString):
+        self.ConnectionString = ConnectionString
+
+if __name__ == "__main__":
+    print('this is data object package')
--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'hello world'
+    c1 = Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    return c1.CustomerName
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
