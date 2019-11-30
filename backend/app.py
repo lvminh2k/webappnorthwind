d41544509953a2c96f7f52298801dadcee1f2d55
lvminh2k <lvminh2k@gmail.com>
2019-11-30 15:51:52 +0700

    add insert db

--- backend/app.py
+++ backend/app.py
@@ -12,19 +12,14 @@ def hello():
     c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
     return c1.CustomerName
 
-@app.route("/create_table")
+@app.route("/test_insert")
 def create_table():
     ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
     c2 = do.Customer(ConnectionString)
-    s1 = c2.createTable()
+    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    s1 = c2.insert(c1)
     return s1
 
-@app.route("/create_db")
-def create_db():
-    ConnectionString = 'user=postgres password=postgres host=10.0.2.15 port=5432'
-    c2 = do.Customer(ConnectionString)
-    s1 = c2.createDB()
-    return s1
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
