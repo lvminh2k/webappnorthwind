8f89a19f42f28693e02d0763585ce5d970681b10
lvminh2k <lvminh2k@gmail.com>
2019-11-30 15:48:54 +0700

    add insert db

--- README.md
+++ README.md
@@ -9,8 +9,16 @@ https://docs.docker.com/install/linux/docker-ce/ubuntu/
 #backend
 sudo docker build -t backend .
 sudo docker run -d --name backend -p 8080:8080 backend
+sudo docker stop backend
+sudo docker rm backend 
 
 #deploy db
 sudo docker load --input postgres_11.2-alpine.tar
 
 sudo docker run -d --restart unless-stopped --name coredb -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -v ~/northwind_db:/var/lib/postgresql/data -p 5432:5432 postgres:11.2-alpine
+
+#postgres
+psql -U postgres
+\l
+create database xyz;
+\c xyz;
--- backend/DataObjects.py
+++ backend/DataObjects.py
@@ -2,34 +2,22 @@ import psycopg2
 class Customer:
     def __init__(self, ConnectionString):
         self.ConnectionString = ConnectionString
-    def createTable(self):
-        #database='postgres-northwind', user='postgres', password='postgres', host="127.0.0.1", port="5432"
+    def insert(self, customer):
         con = None
         try:
             con = psycopg2.connect(self.ConnectionString)
             cur = con.cursor()
-            cur.execute("CREATE TABLE TblCustomers (CustomerID int IDENTITY(1,1) PRIMARY KEY, CustomerName varchar(255), ContactName varchar(255), Address varchar(255), City varchar(255), PostalCode varchar(255), Country varchar(255));")
+            sql = "INSERT INTO TblCustomers(CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (%s, %s, %s, %s, %s, %s)"
+            record_to_insert = (customer.CustomerName, customer.ContactName, customer.Address, customer.City, customer.PostalCode, customer.Country)
+            cur.execute(sql, record_to_insert)
             con.commit()
             con.close()
-            return 'Create TblCustomers successfully'
+            return 'Insert TblCustomers successfully'
         except (Exception, psycopg2.DatabaseError) as error:
             return str(error)
         finally:
             if con is not None:
                 con.close()
-    def createDB(self):
-        con = None
-        try:
-            con = psycopg2.connect(self.ConnectionString)
-            cur = con.cursor()
-            cur.execute("CREATE DATABASE northwind;")
-            con.commit()
-            con.close()
-            return 'Create DB northwind successfully'
-        except (Exception, psycopg2.DatabaseError) as error:
-            return str(error)
-        finally:
-            if con is not None:
-                con.close()
+                
 if __name__ == "__main__":
     print('this is data object package')
