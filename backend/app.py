72aa6f3b122f13eb77dbc9617dca6bb2d24734a0
lvminh2k <lvminh2k@gmail.com>
2019-12-07 13:29:50 +0700

    add get user by id

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import DataObjects as do
 
 app = Flask(__name__)
 
-db_ip = os.getenv("db_ip") #'10.0.2.15'
+db_ip = os.getenv('db_ip') #'10.0.2.15'
 ConnectionData = {}
 ConnectionData['user'] = 'postgres'
 ConnectionData['password'] = 'postgres'
@@ -13,12 +13,11 @@ ConnectionData['host'] = str(db_ip)
 ConnectionData['port'] = '5432'
 ConnectionData['database'] = 'northwind'
 
-@app.route("/")
-def hello():
-    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
-    return c1.CustomerName
+@app.route('/')
+def hello():    
+    return 'this is backend'
 
-@app.route("/test_insert")
+@app.route('/test_insert')
 def test_insert():
     #ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
     c2 = do.Customer(ConnectionData)
@@ -26,6 +25,14 @@ def test_insert():
     s1 = c2.insert(c1)
     return s1
 
+@app.route('/user/get_by_id', methods=['POST'])
+def user_get_by_id():
+    user_id = request.data['user_id']
+    return user_id
+
+@app.route('/user/insert', methods=['POST'])
+def user_insert():
+    return 'ok'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
---
+++ backend/test/test.py
@@ -0,0 +1,6 @@
+import requests
+
+data = {}
+data['user_id'] = 1
+rsp = requests.post('http://172.16.0.198:8080//user/get_by_id', json=data)
+print(rsp.json())
